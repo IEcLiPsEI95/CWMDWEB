2788c2f5bc4f373e44e1af6f9f020385a9344ee5
flilie <flilie@bitdefender.com>
2017-01-17 15:09:56 +0200

    Finished Crud on users

--- assets/scripts/crudusers.js
+++ assets/scripts/crudusers.js
@@ -7,7 +7,10 @@ angular.module('cwmdApp').controller('UserCtrl', function($scope, auth, $rootSco
     }
     $scope.LogoutFunction = function(){
         authToken.removeToken();
-        $window.location.href = "index.html";
+        url = API_URL + "auth/logout";
+        $http.post(url).success(function(res){
+            $window.location.href = "index.html";
+        });
     }
     var url = API_URL + "auth/getallusers";
     $http.get(url)
@@ -19,36 +22,37 @@ angular.module('cwmdApp').controller('UserCtrl', function($scope, auth, $rootSco
         });
     $scope.go = function(x){
         $scope.email = x.username;
-        $scope.first = x.firstname;
-        $scope.last = x.lastname;
-        $scope.permision = x.permision;
+        $scope.first = x.firstName;
+        $scope.last = x.lastName;
+        $scope.permissions = x.permissions;
         $scope.cnp = x.cnp;
         $scope.phone = x.phone;
     }
-    /*$scope.submit = function(){
-        url = API_URL + "auth/" + $scope.type;
-        $http.get(url, {username: $scope.email, password: $scope.password, permision: $scope.permision, firstname: $scope.first, lastname: $scope.last, cnp: $scope.cnp, phone: $scope.phone})
-            .success(function(res){
-                $scope.showAlert = function(ev){
-                    // Appending dialog to document.body to cover sidenav in docs app
-                    // Modal dialogs should fully cover application
-                    // to prevent interaction outside of dialog
-                    $mdDialog.show(
-                      $mdDialog.alert()
-                        .parent(angular.element(document.querySelector('#popupContainer')))
-                        .clickOutsideToClose(true)
-                        .title('This is an alert title')
-                        .textContent('You can specify some description text in here.')
-                        .ariaLabel('Alert Dialog Demo')
-                        .ok('Got it!')
-                        .targetEvent(ev)
-                    );
-                  };
+    $scope.submit = function(){
+        url = API_URL + "auth/";
+        request = angular.element(document.querySelector('#request'))[0].value;
+        if(angular.equals(request,"add"))
+        {
+            url+= "adduser"
+        }
+        if(angular.equals(request,"delete"))
+        {
+            url+= "deluser"
+        }
+        if(angular.equals(request,"update"))
+        {
+            url+= "updateuser"
+        }
+        console.log(url);
+        $http.post(url, {username: $scope.email, password: $scope.password, permissions: $scope.permissions, firstName: $scope.first, lastName: $scope.last, cnp: $scope.cnp, phone: $scope.phone})
+          .success(function(res){
+                    $window.alert("The user has been "+request+"-ed");
+                    $window.location.reload(true);
           })
           .error(function(err){
             console.log(err);
           });
-    };*/
+    };
 });        
            
            
